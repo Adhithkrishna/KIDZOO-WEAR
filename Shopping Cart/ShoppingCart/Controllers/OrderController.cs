06699b644f30fd38a6495a56add903b117adcb0e
Adhithkrishna <adhith.devops@gmail.com>
2021-09-26 01:23:40 +0530

    Order Controller and Order View Model
    Order Controller and Order View Model

---
+++ Shopping Cart/ShoppingCart/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart_DataAccess.Repository.IRepository;
+using ShoppingCart_Models.ViewModels;
+using ShoppingCart_Utility.BrainTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailsRepository _orderDRepo;
+        private readonly IBrainTreeGate _brain;
+
+
+        public OrderController(
+            IOrderHeaderRepository orderHRepo, IOrderDetailsRepository orderDRepo, IBrainTreeGate brain)
+        {
+            _brain = brain;
+            _orderHRepo = orderHRepo;
+            _orderDRepo = orderDRepo;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
